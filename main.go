2f77b888ae4852599a7d3f0019f21fd4254ad42e
Xiaoyu Huang <007pig@gmail.com>
2019-04-24 22:17:09 +0800

    Appserver is working

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"os/user"
 	"path/filepath"
 
+	"github.com/ipfsync/appserver"
+
 	"github.com/ipfsync/ipfsmanager"
 	"go.uber.org/fx"
 )
@@ -40,10 +42,26 @@ func NewIpfsManager(lc fx.Lifecycle) (*ipfsmanager.IpfsManager, error) {
 	return im, nil
 }
 
+func NewAppServer(lc fx.Lifecycle) (*appserver.AppServer, error) {
+	srv := appserver.NewAppServer()
+	lc.Append(fx.Hook{
+		OnStart: func(context.Context) error {
+			srv.Start()
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			return srv.Stop()
+		},
+	})
+
+	return srv, nil
+}
+
 func main() {
 
 	app := fx.New(
 		fx.Invoke(NewIpfsManager),
+		fx.Invoke(NewAppServer),
 	)
 
 	app.Run()
